273ea555883b7d3c03812530efc0d59da3d8b5f1
harry-pm <harrypenderedmazer@gmail.com>
2020-02-06 23:33:17 +0000

    copy in the js game from w3schools

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ class App(web.Application):
         handlers = [
             (r"/", Main_Handler),
             (r"/home", Main_Handler),
-            (r"/game", Game_Handler)
+            (r"/firefly", Firefly_Handler)
         ]
 
         settings = dict(
@@ -21,10 +21,10 @@ class Main_Handler(web.RequestHandler):
     def get(self):
         self.render('homepage.html')
 
-class Game_Handler(web.RequestHandler):
+class Firefly_Handler(web.RequestHandler):
     def get(self):
-        self.render('game.html')
-
+        self.render('firefly.html', script_location = '../static/scripts/firefly.js')
+        
 def main():
     app = App()
     app.listen(8000)
