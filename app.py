0a46344a4cfdc7e0d7e9a6eac40b56e5354dc7b7
harry-pm <harrypenderedmazer@gmail.com>
2020-02-06 17:22:40 +0000

    initial commit, set up skeleton for tornado server

---
+++ app.py
@@ -0,0 +1,29 @@
+import logging
+from tornado import escape, ioloop, web, websocket
+import os.path
+
+class App(web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", Main_Handler)
+        ]
+
+        settings = dict(
+            template_path = os.path.join(os.path.dirname(__file__), "templates"),
+            static_path = os.path.join(os.path.dirname(__file__), "static")
+        )
+
+        super(App, self).__init__(handlers, **settings)
+
+class Main_Handler(web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+def main():
+    app = App()
+    app.listen(8000)
+    print('hi')
+    ioloop.IOLoop.current().start()
+
+if __name__ == "__main__":
+    main()
