d9cf76f4d42c09f881c3ff31a20ff507257d915d
Dominic Holm-Kander <dominic.holmkander@gmail.com>
2020-02-07 11:36:01 +0000

    fix user_and_score model so that pk is autoincremented

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import os.path
 from tornado_sqlalchemy import SQLAlchemy, SessionMixin
 
 ######################################################### Models, SQLAlchemy stuff
-from sqlalchemy import Column, BigInteger, String # for use in defining our models
+from sqlalchemy import Column, BigInteger, String, Integer # for use in defining our models
 
 db = SQLAlchemy(url='sqlite:///test.db')
 
 class User_And_Score(db.Model):
     __tablename__ = "user_and_score"
-    id = Column(BigInteger, primary_key = True)
-    username = Column(String(255), unique = True)
-    snake_highscore = Column(BigInteger) 
+    id = Column(Integer, primary_key = True, autoincrement = True)
+    username = Column(String(255))
+    snake_highscore = Column(Integer) 
     # N.B think this will need restructuring to a table for each game with those highscores
     # rather than storing a score for each game in each player's table - because what if a new game is added? Whole db needs migrating. Keeping for now for speed.
 
@@ -26,6 +26,12 @@ class Snake_Scores_Request_Handler(SessionMixin, web.RequestHandler):
             string = "Current user: " + all_info.username + ". Current score: " + str(all_info.snake_highscore)
             self.write(string)
 
+class Save_Snake_Score_Request_Handler(SessionMixin, web.RequestHandler):
+    def get(self):
+        with self.make_session() as session:
+            session.add(User_And_Score(username = "HPM", snake_highscore = 42))
+            # TODO: add condition to only save score if it is higher than current highest by that user (upsert rather than insert - fix this in a bit!)
+            session.commit()
 
 ############################# Temporarily building DB models in app.py - will abstract out once they are functioning
 
@@ -35,7 +41,8 @@ class App(web.Application):
             (r"/", Main_Handler),
             (r"/home", Main_Handler),
             (r"/game", Game_Handler),
-            (r"/snake_scores", Snake_Scores_Request_Handler)
+            (r"/snake_scores", Snake_Scores_Request_Handler),
+            (r"/save_snake_score", Save_Snake_Score_Request_Handler)
         ]
 
         settings = dict(
