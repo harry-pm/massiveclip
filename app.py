e50e8996364558fa9701e4352977b5a81ca7e8a2
Dominic Holm-Kander <dominic.holmkander@gmail.com>
2020-02-07 10:52:03 +0000

    implement basic database connection

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
 import logging
 from tornado import escape, ioloop, web, websocket
 import os.path
+from tornado_sqlalchemy import SQLAlchemy, SessionMixin
+
+######################################################### Models, SQLAlchemy stuff
+from sqlalchemy import Column, BigInteger, String # for use in defining our models
+
+db = SQLAlchemy(url='sqlite:///test.db')
+
+class User_And_Score(db.Model):
+    __tablename__ = "user_and_score"
+    id = Column(BigInteger, primary_key = True)
+    username = Column(String(255), unique = True)
+    snake_highscore = Column(BigInteger) 
+    # N.B think this will need restructuring to a table for each game with those highscores
+    # rather than storing a score for each game in each player's table - because what if a new game is added? Whole db needs migrating. Keeping for now for speed.
+
+
+
+class DatabaseRequestHandler(SessionMixin, web.RequestHandler):
+    def get(self):
+        with self.make_session() as session:
+            all_info = session.query(User_And_Score).first()
+            string = "Current user: " + all_info.username + ". Current score: " + str(all_info.snake_highscore)
+            self.write(string)
+
+
+############################# Temporarily building DB models in app.py - will abstract out once they are functioning
 
 class App(web.Application):
-    def __init__(self):
+    def __init__(self, db):
         handlers = [
             (r"/", Main_Handler),
             (r"/home", Main_Handler),
-            (r"/game", Game_Handler)
+            (r"/game", Game_Handler),
+            (r"/database", DatabaseRequestHandler)
         ]
 
         settings = dict(
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
-            static_path = os.path.join(os.path.dirname(__file__), "static")
+            static_path = os.path.join(os.path.dirname(__file__), "static"),
+            db = db
         )
 
         super(App, self).__init__(handlers, **settings)
@@ -26,7 +54,7 @@ class Game_Handler(web.RequestHandler):
         self.render('game.html')
 
 def main():
-    app = App()
+    app = App(db=db)
     app.listen(8000)
     print('hi')
     ioloop.IOLoop.current().start()
