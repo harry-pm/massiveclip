ce3cda86e9b7932badf72967b5bb274177709ee6
harry-pm <harrypenderedmazer@gmail.com>
2020-02-07 16:50:37 +0000

    implement simple login functionality without passord

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 from tornado import escape, ioloop, web, websocket
 import os.path
 from tornado_sqlalchemy import SQLAlchemy, SessionMixin
+import tornado
 
 ######################################################### Models, SQLAlchemy stuff
 from sqlalchemy import Column, BigInteger, String, Integer # for use in defining our models
@@ -40,6 +41,7 @@ class App(web.Application):
         handlers = [
             (r"/", Main_Handler),
             (r"/home", Main_Handler),
+            (r"/login", Login_Handler),
             (r"/game", Game_Handler),
             (r"/api/snake_scores", Snake_Scores_Request_Handler),
             (r"/api/save_snake_score", Save_Snake_Score_Request_Handler),
@@ -49,22 +51,42 @@ class App(web.Application):
         settings = dict(
             template_path = os.path.join(os.path.dirname(__file__), "templates"),
             static_path = os.path.join(os.path.dirname(__file__), "static"),
-            db = db
+            db = db,
+            cookie_secret = "__TODO:5",
+            login_url = "/login",
         )
 
         super(App, self).__init__(handlers, **settings)
 
-class Main_Handler(web.RequestHandler):
-    def get(self):
-        self.render('homepage.html')
+class BaseHandler(tornado.web.RequestHandler):
+    def get_current_user(self):
+        return self.get_secure_cookie("user")
 
-class Game_Handler(web.RequestHandler):
+class Login_Handler(BaseHandler):
     def get(self):
-        self.render('game.html', script_location = '../static/scripts/game.js')
+        self.render('login.html')
 
-class Snake_Handler(web.RequestHandler):
+    def post(self):
+        self.set_secure_cookie("user", self.get_argument("name"))
+        self.redirect("/")
+
+class Main_Handler(BaseHandler):
     def get(self):
-        self.render('snake.html', script_location = '../static/scripts/snake.js')
+        if not self.current_user:
+            self.redirect("/login")
+            return 
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.render('homepage.html', name = name)
+
+class Game_Handler(BaseHandler):
+    def get(self):
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.render('game.html', script_location = '../static/scripts/game.js', name = name)
+
+class Snake_Handler(BaseHandler):
+    def get(self):
+        name = tornado.escape.xhtml_escape(self.current_user)
+        self.render('snake.html', script_location = '../static/scripts/snake.js', name = name)
         
 def main():
     app = App(db=db)
