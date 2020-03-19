07d74c956c17c5ce93afad3ca8ea88cc77902094
harry-pm <harrypenderedmazer@gmail.com>
2020-03-19 22:25:50 +0000

    save firefly score to db and update on front end

--- static/scripts/firefly.js
+++ static/scripts/firefly.js
@@ -6,9 +6,8 @@ var myScore;
 myGamePiece = new component(8, 6, "orange", 10, 160, "firefly");
 myGamePiece.gravity = 0.05;
 myScore = new component("30px", "Consolas", "black", 280, 40, "score");
-gameOver = new component("30px", "Consolas", "black", 170, 140, "gameOver");
-playAgain = new component("30px", "Consolas", "black", 50, 180, "playAgain");
-
+gameOverMessage = new component("30px", "Consolas", "black", 170, 140, "gameOverMessage");
+playAgainMessage = new component("30px", "Consolas", "black", 70, 180, "playAgain");
 
 
 //flying control
@@ -69,7 +68,7 @@ function component(width, height, color, x, y, type) {
             ctx.fill();
 
         //draw gamover
-        } else if (this.type == "gameOver") {
+        } else if (this.type == "gameOverMessage") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText("Gameover", this.x, this.y);
@@ -78,7 +77,7 @@ function component(width, height, color, x, y, type) {
         } else if (this.type == "playAgain") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
-            ctx.fillText("Press any key to play again", this.x, this.y);
+            ctx.fillText("Press space to play again", this.x, this.y);
 
         //draw rectangles
         } else {
@@ -119,11 +118,53 @@ function component(width, height, color, x, y, type) {
 function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {
+
+        //if the firefly crashes
         if (myGamePiece.crashWith(myObstacles[i])) {
-            //add functionality here to send score to db
             myGameArea.stop();
-            gameOver.update();
-            playAgain.update();
+            gameOverMessage.update();
+            playAgainMessage.update();
+
+            //save the score
+            let xhr = new XMLHttpRequest();
+            let username = document.getElementById('name').innerHTML;
+            let score = myGameArea.frameNo;
+
+            //update the user high score
+            current_highscore = document.getElementById("score").innerHTML;
+            if (score > current_highscore || current_highscore === "No highscore yet!") {
+                document.getElementById("score").innerHTML = score;
+            }
+
+            //update table
+            row_username = document.getElementsByClassName("row_username");
+            row_score = document.getElementsByClassName("row_score");
+            
+            let users_and_scores = [[username, score]];
+            for (let i = 0; i < row_username.length; i++) {
+                users_and_scores.push([row_username[i].innerHTML, parseInt(row_score[i].innerHTML)])
+            };
+
+            users_and_scores.sort((a,b) => b[1] - a[1]);
+            if (users_and_scores.length > 5) {
+                users_and_scores = users_and_scores.slice(0,5);
+            };
+
+            let html_array = [];
+            for (let i = 0; i < users_and_scores.length; i ++) {
+                html_array.push(`<tr><td>${i + 1}</td><td class="row_username">${users_and_scores[i][0]}</td><td class="row_score">${users_and_scores[i][1]}</td></tr>`)
+            };
+
+            document.getElementById("highscores_table").innerHTML = html_array.join("");
+
+            //FOR MASTER BRANCH
+            // xhr.open("GET", 'http://massiveclip.herokuapp.com/api/save_firefly_score?'+"username=" + username + "&" + "firefly_score=" + score , true);
+
+            //FOR DEVELOPMENT
+            xhr.open("GET", 'http://localhost:8000/api/save_firefly_score?'+"username=" + username + "&" + "firefly_score=" + myGameArea.frameNo, true);
+
+            xhr.send();
+
             return;
         } 
     }
@@ -158,8 +199,6 @@ window.addEventListener('keydown', function (e) {
         myObstacles = [];
         myGameArea.clear();
         myGameArea.start();
-        this.console.log('go again')
-        // myGameArea.start();
     };
 });
 
